c1727d000b6108161a77f7d73d177d6cd4e80f75
bryanederson19 <bryanederson19@gmail.com>
2025-04-06 21:07:21 +0000

    começo do js

--- main.js
+++ main.js
@@ -1,13 +1,4 @@
-let bill = 0;
-let tipPercentage = 0;
-let numberOfPeople = 0;
-let buttonSelected = null;
 
-let billInput = document.querySelector('#bill');
-billInput.addEventListener("input", reciveBillValue)
-
-let numberOfPeopleInput = document.querySelector('#people');
-numberOfPeopleInput.addEventListener("input", reciveNumberOfPeopleValue)
 
 function reciveBillValue() {
     bill = billInput.valueAsNumber;
